2a867c311ae23c925c41e8abe9384ed4b594d666
TobiasTLRT <133592076+TobiasTLRT@users.noreply.github.com>
2023-11-26 14:31:14 -0300

    Add files via upload

---
+++ backend/clase_N3/index.php
@@ -0,0 +1,39 @@
+<?php
+echo "Hola mundo";
+echo"<br>"; 
+$string="hola mundo";
+echo $string;
+
+$variable1=14;
+$variable2=7;
+echo"<br>";
+echo $variable1 + $variable2;
+echo"<br>";
+echo $variable1 - $variable2;
+echo"<br>";
+echo $variable1 * $variable2;
+echo"<br>";
+echo $variable1 / $variable2;
+echo"<br>";
+echo $variable1 % $variable2;
+
+echo"<br>";
+$celsius = 20;
+    echo $fahrenheit = ($celsius * 9/5) + 32;
+    echo "<br>";
+
+    $baseRectangulo = 18;
+    $alturaRectangulo = 12;
+    
+    // Área del rectángulo
+    $areaRectangulo = $baseRectangulo * $alturaRectangulo;
+    
+    // Perímetro del rectángulo
+    $perimetroRectangulo = 2 * ($baseRectangulo + $alturaRectangulo);
+    echo "<br>";
+    echo $areaRectangulo;
+    echo "<br>";
+    echo $perimetroRectangulo
+    
+    
+?>
